56a394d99b5db3f4c3a5b95aa4030b89b22cd2b4
KevalSardhara <kevalsardhara9@gmail.com>
2024-09-21 13:00:03 +0530

    add(): text

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,8 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more about the application development platform also deployment the code on AWS
+        Click on the Vite and React logos to learn more about the application development platform also deployment the code on AWS Cloud
+        
       </p>
     </>
   )
